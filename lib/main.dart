07f56ac43075ba70dbe3c3d177832f98a474175c
Shane Lenagh <shanelenagh@gmail.com>
2024-02-26 15:00:18 -0600

    Class-encapsulated sequential player example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -6,7 +5,11 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+interface class SequencePlayCompletionListner {
+  void sequencePlayCompletion() { }
+}
+
+class MainApp extends StatelessWidget implements SequencePlayCompletionListner {
   const MainApp({super.key});
 
   @override
@@ -25,17 +28,45 @@ class MainApp extends StatelessWidget {
 
   void playIt() async {
     AudioCache.instance.prefix = "assets/audio/";
-    AudioCache.instance.loadAll([ /*"example_web_c-c-1.mp3",*/ "tr_cl_chirp.mp3", "tr_bogey.mp3" ]);
-    //final playerApplause = AudioPlayer();
-    //playerApplause.setSource(AssetSource("example_web_c-c-1.mp3"));
-    //await playerApplause.resume();
-    final chirpPlayer = AudioPlayer(), bogeyPlayer = AudioPlayer();
+    await AudioCache.instance.loadAll([ "tr_cl_chirp.mp3", "tr_bogey.mp3", "tr_01.mp3", "tr_oclock.mp3", "tr_high.mp3" ]);
+
+    final chirpPlayer = AudioPlayer(), bogeyPlayer = AudioPlayer(), onePlayer = AudioPlayer(), 
+      oclockPlayer = AudioPlayer(), highPlayer = AudioPlayer();
     chirpPlayer.setSource(AssetSource("tr_cl_chirp.mp3"));
     bogeyPlayer.setSource(AssetSource("tr_bogey.mp3"));
-    //await chirpPlayer.resume();
-    bogeyPlayer.onPlayerComplete.listen((event) {
-      chirpPlayer.resume();  
-    });
-    bogeyPlayer.resume(); 
+    onePlayer.setSource(AssetSource("tr_01.mp3"));
+    oclockPlayer.setSource(AssetSource("tr_oclock.mp3"));
+    highPlayer.setSource(AssetSource("tr_high.mp3"));
+
+    AudioSequencePlayer([ chirpPlayer, bogeyPlayer, onePlayer, oclockPlayer, highPlayer ], this).playAudioSequence();
+  }
+
+  @override
+  void sequencePlayCompletion() { 
+    print("Done playing sequence");
+  }
+}
+
+class AudioSequencePlayer {
+  final List<AudioPlayer> _audioPlayers;
+
+  AudioSequencePlayer(List<AudioPlayer> audioPlayers, [SequencePlayCompletionListner? sequenceCompletionListener ]) 
+    : _audioPlayers = audioPlayers, assert(audioPlayers.isNotEmpty)
+  {
+    for (int i = 0; i < _audioPlayers.length; i++) {
+      if (i < _audioPlayers.length-1) {
+        _audioPlayers.elementAt(i).onPlayerComplete.listen((event) {
+          _audioPlayers.elementAt(i+1).resume();
+        });
+      } else if (sequenceCompletionListener != null) {
+        _audioPlayers.elementAt(i).onPlayerComplete.listen((event) {
+          sequenceCompletionListener.sequencePlayCompletion();
+        });
+      }
+    }
+  }
+
+  void playAudioSequence() {
+    _audioPlayers.elementAt(0).resume();
   }
 }
